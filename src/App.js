783cf2668f01c8b5ec7a324db4c146a0631afe46
Jason Rhoads <jasrhoads@gmail.com>
2024-02-01 16:18:50 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+import TopNav from './TopNav';
+import HeroSection from './HeroSection';
+import PortfolioSection from './PortfolioSection';
+
+import { 
+  OuterContainer, 
+  SectionContainer, 
+  SectionHeader, 
+  SectionHeaderBackground, 
+  // SectionContentContainer,
+} from './StyledComponents';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <TopNav></TopNav>
+    <OuterContainer>
+      <HeroSection />
+      <PortfolioSection />
+      
+      <SectionContainer id="about" className='section-container'>
+        <SectionHeaderBackground />
+        <SectionHeader>
+          <h2 className='h1'>About</h2>
+        </SectionHeader>
+          <h3 className='h1'>Born to build</h3>
+          <p>I have always loved building things, from Legos, model cars, miniatures, and websites. Exciting new technologies have emerged over the years that allow people to create their own unique space on the internet. I am here to help you with yours!</p>
+      </SectionContainer>
+      <SectionContainer id="contact" className='section-container'>
+        <SectionHeaderBackground />
+        <SectionHeader>
+         <h2 className='h1'>Contact</h2>
+        </SectionHeader>
+        Contact
+      </SectionContainer>
+    </OuterContainer>
+    </>
   );
 }
 
