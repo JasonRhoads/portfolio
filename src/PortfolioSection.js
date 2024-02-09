373b43431024cfb5f6d7358b45c4be00afc37023
Jason Rhoads <jasrhoads@gmail.com>
2024-02-09 09:27:55 -0800

    add Modal to portfolio

--- src/PortfolioSection.js
+++ src/PortfolioSection.js
@@ -15,6 +15,14 @@ import order_summary_mobile from './images/order_summary_mobile.jpg'
 import portraits_jwr from './images/portraits_jwr.jpg'
 import shopify_store from './images/shopify_store_home_screen.jpg'
 
+
+import * as React from 'react';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
 import  {
     SectionContainer, 
     SectionHeader, 
@@ -68,23 +76,59 @@ let portfolio = {
 }
 
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 export default function PortfolioSection() {
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+
     return (
         <SectionContainer id="portfolio" className='section-container'>
         <SectionHeaderBackground />
         <SectionHeader>
           <h2 className='h1'>Portfolio</h2>
         </SectionHeader>
-        {/* spacing={{xs: 2, md: 3, lg: 5}} */}
-        <Grid className="portfolio-grid" container > 
+        <Grid className="portfolio-grid" container spacing={{xs: 2}}> 
             {Object.keys(portfolio).map((project) => (
                 <Grid xs={12} sm={6} md={3} key={project}>
-                    <Link href={portfolio[project].link} target="_blank">
-                        <Paper className='grid-item'>
-                            <img className="portfolio-image" src={portfolio[project].image} alt={portfolio[project].name}/>
-                            <p>{portfolio[project].name}</p> 
-                        </Paper>
-                    </Link>
+                    <Paper className='grid-item' sx={{
+                            backgroundImage: `url(${portfolio[project].image})`,
+                            backgroundSize: 'auto 100%',
+                            backgroundPosition: 'center',}}
+                            alt={portfolio[project].name}>
+                        <div className='modal-container'>
+                            <Button className="btn btn-learn-more" onClick={handleOpen}>Learn More</Button>
+                            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                            >
+                                <Box sx={style}>
+                                <Link href={portfolio[project].link} target="_blank"><p>{portfolio[project].name}</p></Link>
+                                <Typography id="modal-modal-title" variant="h6" component="h2">
+                                    {portfolio[project].name}
+                                </Typography>
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                                </Typography>
+                                </Box>
+                            </Modal>
+                        </div>
+                    </Paper>
                 </Grid>
             ))}  
         </Grid>
